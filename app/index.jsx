e82a7da03bf8c4e376728d3c87de40ee0932af3b
Reese Wilson <reese@chicagobooth.edu>
2024-12-17 16:20:56 -0600

    Initial commit - Expo app setup

---
+++ app/index.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Bluebook Cite</Text>
+    </View>
+  );
+}
+
+const s = StyleSheet.create({
+
+});
