089ee7ff5e00dc7f722574467b3abb894ac05d54
Reese Wilson <reese@chicagobooth.edu>
2024-12-23 23:12:34 -0600

    fixed issue with commented out FileSystem

--- utils/fileHandlers.js
+++ utils/fileHandlers.js
@@ -1,7 +1,7 @@
 import * as ImagePicker from "expo-image-picker";
 import * as DocumentPicker from "expo-document-picker";
 import { Platform } from "react-native";
-// import * as FileSystem from "expo-file-system";
+import * as FileSystem from "expo-file-system";
 
 export const handleSelectPhoto = async (setFileUri, setFileSize) => {
   try {
